6264b35343cf886fc925364ed42e175246628a0c
FocusedBit <anthonyrayhollis@gmail.com>
2019-09-26 07:13:21 -0400

    Adding Search and Replace

---
+++ FCC/IntermediateAlgo/SearchAndReplace.js
@@ -0,0 +1,41 @@
+/*Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note
+Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"*/
+
+function myReplace(str, before, after) {
+    let arr = str.split(" ");
+    let testBefore = before.split("");
+    let alterAfter = after.split("");
+
+    if (testBefore[0] === testBefore[0].toUpperCase()) {
+        alterAfter.splice(0, 1, alterAfter[0].toUpperCase());
+    }
+
+    arr.splice(arr.indexOf(before), 1, alterAfter.join(""));
+
+    return arr.join(" ");
+/*Alternate:
+    // Find index where before is on string
+    var index = str.indexOf(before);
+    // Check to see if the first letter is uppercase or not
+    if (str[index] === str[index].toUpperCase()) {
+        // Change the after word to be capitalized before we use it.
+        after = after.charAt(0).toUpperCase() + after.slice(1);
+    }
+    // Now replace the original str with the edited one.
+    str = str.replace(before, after);
+
+    return str;
+*/
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("Let us go to the store", "store", "mall");
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
